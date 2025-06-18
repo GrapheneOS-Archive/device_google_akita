89c6b83adccb40fa52ec7855ef2535a5612851bc
Fred Underwood <underwoodfred@proton.me>
2025-06-18 14:46:58 +1000

    remove property from location

--- location/gnssd/gnss_release.mk
+++ location/gnssd/gnss_release.mk
@@ -17,8 +17,6 @@ DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
     PRODUCT_COPY_FILES += \
         device/google/akita/location/gnssd/release/gps.cfg:vendor/etc/gnss/gps.cfg
-    PRODUCT_VENDOR_PROPERTIES += \
-        vendor.gps.aol.enabled=true
 else
     PRODUCT_COPY_FILES += \
         device/google/akita/location/gnssd/release/gps_user.cfg:vendor/etc/gnss/gps.cfg
