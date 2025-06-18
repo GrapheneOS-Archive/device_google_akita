aa0a70ff67d7d1d87c58ffa19c51d3ca11ef12ea
Fred Underwood <underwoodfred@proton.me>
2025-06-18 15:00:44 +1000

    strip unused namespace

--- device-akita.mk
+++ device-akita.mk
@@ -77,10 +77,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
 
-# Include Bluetooth soong namespace
-PRODUCT_SOONG_NAMESPACES += \
-    device/google/akita/bluetooth
-
 # Enable one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
